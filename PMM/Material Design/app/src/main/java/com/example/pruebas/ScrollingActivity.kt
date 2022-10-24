95e9cce92e077676645300c595585f185f1f0231
FsantaG <fsantagestiones@gmail.com>
2022-10-24 12:50:51 +0200

    Pruebas Material Design (Y alguna otra cosa de DDI)

--- PMM/Material Design/app/src/main/java/com/example/pruebas/ScrollingActivity.kt
+++ PMM/Material Design/app/src/main/java/com/example/pruebas/ScrollingActivity.kt
@@ -8,39 +8,29 @@ import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
 import com.example.pruebas.databinding.ActivityScrollingBinding
+import com.google.android.material.bottomappbar.BottomAppBar
 
 class ScrollingActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityScrollingBinding
+    private lateinit var binding:ActivityScrollingBinding;
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+        super.onCreate(savedInstanceState);
 
-        binding = ActivityScrollingBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        binding=ActivityScrollingBinding.inflate(layoutInflater);
+        setContentView(binding.root);
 
-        setSupportActionBar(findViewById(R.id.toolbar))
-        binding.toolbarLayout.title = title
-        binding.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
+        binding.btnAdd.setOnClickListener {
+            if(binding.barra.fabAlignmentMode == BottomAppBar.FAB_ALIGNMENT_MODE_END){
+                binding.barra.fabAlignmentMode = BottomAppBar.FAB_ALIGNMENT_MODE_CENTER;
+            }else{
+                binding.barra.fabAlignmentMode = BottomAppBar.FAB_ALIGNMENT_MODE_END
+            }
+            binding.barra.setNavigationOnClickListener{
+                Snackbar.make(binding.root, "pito", Snackbar.LENGTH_LONG).setAnchorView(binding.btnAdd).show();
+            }
+        };
     }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_scrolling, menu)
-        return true
-    }
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
-        }
-    }
 }
