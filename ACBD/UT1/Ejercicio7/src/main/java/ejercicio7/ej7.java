5fb6ae3fad9c208ef689dbd188af8511ac383ff4
FsantaG <fsantagestiones@gmail.com>
2022-10-24 12:51:33 +0200

    Ejercicio 7 ACBD (WIP)

---
+++ ACBD/UT1/Ejercicio7/src/main/java/ejercicio7/ej7.java
@@ -0,0 +1,19 @@
+/*
+*Crea un nuevo proyecto en Java llamado Ejercicio7:
+*
+*   o Crea tu propio ejercicio aplicando las clases que hemos visto para trabajar con ficheros binarios.
+*   o Al inicio del fichero java expresa mediante un comentario lo que hace el programa. 
+*     Comenta sobre el código lo necesario para que se entienda.
+*/
+package ejercicio7;
+
+/**
+ *
+ * @author dam
+ */
+public class ej7 {
+    public static void main(String[] args) {
+        
+        
+    }
+}
