3b06a6cbf0b6a9f7d60d328a00e922c84bf5b670
FsantaG <fsantagestiones@gmail.com>
2022-10-25 11:34:06 +0200

    Ejercicio 1 MAterial Design

--- PMM/Material Design/app/src/main/java/com/example/pruebas/ScrollingActivity.kt
+++ PMM/Material Design/app/src/main/java/com/example/pruebas/ScrollingActivity.kt
@@ -7,27 +7,34 @@ import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
 import com.example.pruebas.databinding.ActivityScrollingBinding
 import com.google.android.material.bottomappbar.BottomAppBar
 
 class ScrollingActivity : AppCompatActivity() {
 
-    private lateinit var binding:ActivityScrollingBinding;
+    private lateinit var binding: ActivityScrollingBinding;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState);
 
-        binding=ActivityScrollingBinding.inflate(layoutInflater);
+        binding = ActivityScrollingBinding.inflate(layoutInflater);
         setContentView(binding.root);
 
         binding.btnAdd.setOnClickListener {
-            if(binding.barra.fabAlignmentMode == BottomAppBar.FAB_ALIGNMENT_MODE_END){
+            if (binding.barra.fabAlignmentMode == BottomAppBar.FAB_ALIGNMENT_MODE_END) {
                 binding.barra.fabAlignmentMode = BottomAppBar.FAB_ALIGNMENT_MODE_CENTER;
-            }else{
+            } else {
                 binding.barra.fabAlignmentMode = BottomAppBar.FAB_ALIGNMENT_MODE_END
             }
-            binding.barra.setNavigationOnClickListener{
-                Snackbar.make(binding.root, "pito", Snackbar.LENGTH_LONG).setAnchorView(binding.btnAdd).show();
+            binding.barra.setNavigationOnClickListener {
+                Snackbar.make(
+                    binding.root,
+                    getString(R.string.buttonContentDesc),
+                    Snackbar.LENGTH_LONG
+                ).setAnchorView(binding.btnAdd).setAction("Botón Acción") {
+                    Toast.makeText(this, "Mensaje Toast", Toast.LENGTH_LONG).show()
+                }.show();
             }
         };
     }
