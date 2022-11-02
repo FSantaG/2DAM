027afe9ffaf74ed22b8f3a0cf1a5e6343f7bdb51
FsantaG <fsantagestiones@gmail.com>
2022-11-02 08:34:15 +0100

    Ejercicio de Diseño de Interfaces + Ej 7 ACBD

--- ACBD/UT1/Ejercicio7/src/main/java/ejercicio7/ej7.java
+++ ACBD/UT1/Ejercicio7/src/main/java/ejercicio7/ej7.java
@@ -6,14 +6,64 @@
 *     Comenta sobre el código lo necesario para que se entienda.
 */
 package ejercicio7;
+import java.io.*;
+import java.util.*;
 
 /**
- *
- * @author dam
+ * Escritura de un archivo de texto mediante interacción directa del usuario con la consola, 
+ * y lectura del mismo para comprobación del usuario de que sus datos han sido correctamente introducidos en
+ * el archivo de texto.
+ * @author Frenando Santamaria
  */
 public class ej7 {
     public static void main(String[] args) {
-        
-        
+        Boolean salir = false;
+        String textoFinal = "";
+        File log = new File("log.dat");
+           try{
+               do{ 
+                   Scanner scanner = new Scanner(System.in);
+                   FileOutputStream archivoTexto = new FileOutputStream(log);
+                   FileInputStream archivoTextoR = new FileInputStream(log);
+                   RandomAccessFile archivoRandom = new RandomAccessFile(log, "rw");
+               
+                   System.out.println("Escriba algo:");
+                   String texto = scanner.nextLine();
+               
+                   textoFinal += texto + "\n";  
+                   System.out.println("¿Desea seguir? S/N");
+                   String checkSalida = scanner.nextLine();
+                   
+                   switch(checkSalida){
+                       case "S":
+                       case "s":                      
+                           break;
+                       case "N":
+                       case "n":
+                           byte caracteres[] = textoFinal.getBytes();
+                           archivoTexto.write(caracteres);
+                           archivoTexto.close();
+                           System.out.println("Texto guardado:");
+                           int i=0;    
+                           while((i=archivoTextoR.read())!=-1){    
+                                System.out.print((char)i);
+                           }
+                           archivoTextoR.close();
+                           
+                           System.out.println("Longitud del archivo: " + archivoRandom.length());
+                           archivoRandom.close();
+                           
+                           salir = true;
+                           break;
+                       default:
+                           System.out.println("El programa se ha cerrado");
+                           archivoTexto.close();
+                           salir=true;
+                           break;
+                   }
+               }while(!salir);
+           }catch(Exception e){
+               System.out.println("Se ha producido un error: " + e.getMessage());
+           }
     }
 }
---
+++ DDI/UT2/Ejercicio 3/Ejercicio 3/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejercicio_3
+{
+    public partial class Form1 : Form
+    {
+        String[] callesDisponibles = { "Calle San José", "Calle Ramón y Cajal",
+        "Calle Burguense", "Calle Molinillo", "Calle Andrés Martinez Zatorre",
+        "Calle Santa Clara", "Calle Mateo Cerezo", "Calle Dr. Fleming",
+        "Calle Padre Salaverri", "Calle Diego de Siloe", "Calle Cartuja de Miraflores",
+        "Calle Hijos de Santiago Rodriguez", "Calle Nicolás de Vergara",
+        "Calle Obispo Don Mauricio" };
+        public Form1()
+        {
+            InitializeComponent();
+
+            menuNormal.Checked = false;
+            menuVegano.Checked = false;
+            rellenarCalles();
+        }
+        private void nombre_TextChanged(object sender, EventArgs e)
+        {
+            char[] caracteres = nombre.Text.ToCharArray();
+            if (!checkCaracteres(caracteres))
+            {
+                errorSeleccion.SetError(nombre, "Por favor, introduzca sólo letras");
+                deshabilitarBtnReserva();
+            }
+            else
+            {
+                errorSeleccion.SetError(nombre, "");
+                habilitarBtnReserva();
+            }
+        }
+
+        private void telefono_TextChanged(object sender, EventArgs e)
+        {
+            char[] numTelefono = telefono.Text.ToCharArray();
+            if (checkCaracteres(numTelefono))
+            {
+                errorSeleccion.SetError(telefono, "Por favor, introduzca sólo caracteres numéricos");
+                deshabilitarBtnReserva();
+            }
+            else
+            {
+                errorSeleccion.SetError(telefono, "");
+                habilitarBtnReserva();
+            }
+        }
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        //Métodos Propios
+        private void habilitarBtnReserva()
+        {
+            if((menuNormal.Checked || menuVegano.Checked) && nombre.Text != "" 
+                && telefono.Text != "" && selectCalle.SelectedIndex != -1 
+                && num.Text != "" && piso.Text != "" && letra.Text != "")
+            {
+                btnReserva.Enabled = true;
+            }
+            else
+            {
+                btnReserva.Enabled = false;
+            }
+        }
+
+        private void deshabilitarBtnReserva()
+        {
+            btnReserva.Enabled = false;
+        }
+
+        private void rellenarCalles()
+        {
+            for(int i = 0; i < callesDisponibles.Length; i++)
+            {
+                selectCalle.Items.Add(callesDisponibles[i]);
+            }
+        }
+        //TODO
+        //Modificar esto, y hacer 2 verificadores separados
+        private Boolean checkCaracteres(char[] caracteres)
+        {
+            for (int i = 0; i < caracteres.Length; i++)
+            {
+                if (!Char.IsLetter(caracteres[i]))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        
+    }
+}
