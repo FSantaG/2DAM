44766b2c07fb979a99b91afdde5d16e0f8373b15
FSantaG <fsantagestiones@gmail.com>
2022-11-03 18:18:56 +0100

    Odio mi vida

--- PMM/ut4MD_SantamariaFernando/E2MDSantamariaFernando/app/src/main/java/com/example/e2mdsantamariafernando/MainActivity.kt
+++ PMM/ut4MD_SantamariaFernando/E2MDSantamariaFernando/app/src/main/java/com/example/e2mdsantamariafernando/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.e2mdsantamariafernando
 
+import android.content.Context
 import android.os.Bundle
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
@@ -9,17 +10,21 @@ import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
 import com.example.e2mdsantamariafernando.databinding.ActivityMainBinding
+import com.example.e2mdsantamariafernando.databinding.FragmentFirstBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
+    private lateinit var bindingFirstFragment: FragmentFirstBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
+        bindingFirstFragment = FragmentFirstBinding.inflate(layoutInflater);
         setContentView(binding.root)
 
         setSupportActionBar(binding.toolbar)
@@ -29,7 +34,12 @@ class MainActivity : AppCompatActivity() {
         setupActionBarWithNavController(navController, appBarConfiguration)
 
         binding.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+            Snackbar.make(view, "Creado por: Fernando Santamaría", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+
+        bindingFirstFragment.btn0.setOnClickListener{ view ->
+            Snackbar.make(view, "Pito", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
         }
     }
@@ -40,15 +50,15 @@ class MainActivity : AppCompatActivity() {
         return true
     }
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+    /*override fun onOptionsItemSelected(item: MenuItem): Boolean {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         return when (item.itemId) {
-            R.id.action_settings -> true
+            //R.id.action_settings -> true
             else -> super.onOptionsItemSelected(item)
         }
-    }
+    }*/
 
     override fun onSupportNavigateUp(): Boolean {
         val navController = findNavController(R.id.nav_host_fragment_content_main)
