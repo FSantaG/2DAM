018f8da288adfa081c7efb8e8e4562d8fa3ce848
FsantaG <fsantagestiones@gmail.com>
2022-11-07 12:46:56 +0100

    Cosas PMM

--- PMM/Material Design/app/src/main/java/com/example/pruebas/ScrollingActivity.kt
+++ PMM/Material Design/app/src/main/java/com/example/pruebas/ScrollingActivity.kt
@@ -7,7 +7,11 @@ import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
+import android.webkit.URLUtil
 import android.widget.Toast
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.example.pruebas.databinding.ActivityScrollingBinding
 import com.google.android.material.bottomappbar.BottomAppBar
 
@@ -37,6 +41,33 @@ class ScrollingActivity : AppCompatActivity() {
                 }.show();
             }
         };
+        cargarImagen();
+        binding.content.etUrl.setOnFocusChangeListener{ view, focus ->
+            val url = binding.content.etUrl.text.toString();
+            var error:String?=null;
+            if(!focus){
+                if(url.isEmpty()){
+                    error = getString(R.string.espacioVacio);
+                }else{
+                    if(URLUtil.isValidUrl(url)){
+                        cargarImagen(url);
+                        error = null;
+                    }else{
+                        error = getString(R.string.direccion);
+                    }
+                }
+                binding.content.etUrl.error = error;
+                binding.content.titURL.error = error;
+            }
+        }
+    }
+
+    private fun cargarImagen(url:String="https://fs-prod-cdn.nintendo-europe.com/media/images/08_content_images/games_6/nintendo_switch_7/nswitch_kirbyandtheforgottenland/KirbyAndTheForgottenLand_Intro_SideImg_Kirby.png"){
+        Glide.with(this)
+            .load(url)
+            //.centerCrop()
+            .diskCacheStrategy(DiskCacheStrategy.ALL)//ALL -> Guarda imagen real + modificada
+            .into(binding.content.kirbymagen)
     }
 
 
