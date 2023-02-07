3a310bd603c88f9c34132832c36db7b5613c5a75
FsantaG <fsantagestiones@gmail.com>
2023-02-07 13:55:28 +0100

    Cosas

---
+++ DDI/UT4/Repaso2daEvaluacion/Repaso2daEvaluacion/Fichar.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Repaso2daEvaluacion
+{
+    public partial class Fichar : Form
+    {
+        public Fichar()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            txtFecha.Text = DateTime.Now.ToString();
+        }
+    }
+}
